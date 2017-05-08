839b34e0c12c0d02a7642ee74c1055f84749a97b
shanebergman <shane.bergman@tamarackconsultingllc.com>
2017-05-08 11:58:50 -0600

    test

--- app.js
+++ app.js
@@ -44,7 +44,6 @@ function doScrape(){
     )
 }
 
-doScrape()
 
 var CronJob = require('cron').CronJob;
 var job = new CronJob({
@@ -59,6 +58,6 @@ var job = new CronJob({
   start: false,
   timeZone: 'America/Los_Angeles'
 });
-//job.start();
+job.start();
 
 module.exports = app;
